27fbd6caeb1e8a01d995fcba40ccbe6d22c6b825
ziyadbch22 <ziyadbouibcha@gmail.com>
2025-11-05 09:25:49 +0100

    Add route annotation to LuckyController for number generation

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 
 class LuckyController
 {
+    #[Route('/lucky/number')]
     public function number(): Response
     {
         $number = random_int(0, 100);
